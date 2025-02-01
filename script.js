bd332f568e2458d6ead88552a4a2f66c55fce16f
natnat29-star <marklisteraljas.202300048@gmail.com>
2025-02-01 11:14:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Variables
+let name = "Mark";
+let age = 22;
+let gender = "Male";
+let isStudent = true;
+console.log(`Name: ${name}, Age: ${age}, Gender: ${gender}, Student: ${isStudent}`);
+
+
+for (let i = 1; i <= 10; i++) {
+ console.log(i);
+}
+
+
+function addNumbers(a, b) {
+ return a + b;
+}
+console.log(addNumbers(5, 10));
+
+
+if (age >= 22) {
+ console.log("Adult");
+} else {
+ console.log("Minor");
+}
